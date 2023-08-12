1216f8ffba6745b1217938a049d8437a55da264d
br14nn <vituallabrian727@gmail.com>
2023-08-12 18:39:42 +0800

    fixed responsiveness in LinksGroup

--- src/components/LinksGroup/LinksGroup.tsx
+++ src/components/LinksGroup/LinksGroup.tsx
@@ -5,7 +5,7 @@ type TLinksGroupProps = {
 
 export default function (props: TLinksGroupProps) {
   return (
-    <div className="flex w-[15%] flex-col items-start gap-y-2">
+    <div className="flex w-[15%] flex-col items-start gap-y-2 mobile-max:w-full">
       <p className="text-xl font-bold">{props.title}</p>
       {props.children}
     </div>
